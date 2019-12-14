1a62d014317a715c9c1ecdacc715e9845003c5d1
Gaurav Munjal <Gaurav0@aol.com>
2019-12-14 07:01:59 -0500

    obfuscate token with xor

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import { context } from "@actions/github";
 import { WebhookPayloadPullRequest } from "@octokit/webhooks";
 
 import { backport } from "./backport";
+import { xorHexStrings } from "./xor";
 
 const run = async () => {
   try {
     const token = getInput("github_token", { required: true });
     const botUsername = getInput("bot_username", { required: true });
-    const botToken = getInput("bot_token", { required: true });
+    const encryptedBotToken = getInput("bot_token", { required: true });
+    const botTokenKey = getInput("bot_token_key", { required: true });
+    const botToken = xorHexStrings(encryptedBotToken, botTokenKey);
     debug(JSON.stringify(context, null, 2));
     await backport({
       botToken,
